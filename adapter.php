5d359514feb6b2f966d74ed22ba1cd7336ec7eac
Iryna Prokopenko <iryna.prokopenko@theiconic.com.au>
2018-10-06 10:53:34 +1000

    Add example of Adapter pattern

---
+++ adapter.php
@@ -0,0 +1,62 @@
+<?php
+
+
+interface Adapter
+{
+
+    public function sent();
+
+}
+
+class BraintreeAdapter implements Adapter
+{
+
+    //some members or attributes here
+
+    public function sent()
+    {
+        print ('send data to braintree');
+        // TODO: Implement sent() method.
+    }
+}
+
+
+class PayflowAdapter implements Adapter
+{
+
+    public function sent()
+    {
+        echo('send data tp payflow');
+        // TODO: Implement sent() method.
+    }
+}
+
+
+class Payment
+{
+    protected $payment;
+
+    public function __construct($payment = null)
+    {
+        $this->payment = $payment;
+    }
+
+    public function doPayment()
+    {
+        if (!$this->payment) {
+
+           $do = new BraintreeAdapter();
+           $do->sent();
+           return;
+        }
+
+        $do = new PayflowAdapter();
+        $do->sent();
+        return;
+
+    }
+}
+
+$test = new Payment('visa');
+
+$test->doPayment();
