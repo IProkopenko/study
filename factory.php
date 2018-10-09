f7f07ff1e91386f725dd3f10e9b5f6afa6ef47ba
Iryna Prokopenko <iryna.prokopenko@theiconic.com.au>
2018-10-09 16:14:56 +1100

    add Factory class with examples

--- factory.php
+++ factory.php
@@ -5,3 +5,40 @@
  * Date: 6/10/18
  * Time: 10:26 AM
  */
+
+class Australia
+{
+    const CONSTANT = 'Australia';
+}
+
+class Norway
+{
+    const CONSTANT = 'Norway';
+}
+
+class CountryFactory
+{
+
+    /*
+     * @throws \Exception
+     */
+    public static function createBill($country = null)
+    {
+        if ($country == null) {
+            throw new Exception('Please provide country code');
+        }
+        if ($country == 'Australia') {
+
+            return new Australia();
+        }
+        if ($country == 'Norway') {
+
+            return new Norway();
+        }
+    }
+}
+
+//$myBills = CountryFactory::createBill();
+
+echo CountryFactory::createBill('Norway')::CONSTANT;
+
